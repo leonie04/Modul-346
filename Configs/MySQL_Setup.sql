6464570838d174fe30e4bf72e7be3780fcd2b274
leonie04 <143601194+leonie04@users.noreply.github.com>
2023-12-09 16:53:42 +0100

    Update MySQL_Setup.sql

--- Configs/MySQL_Setup.sql
+++ Configs/MySQL_Setup.sql
@@ -1,3 +1,14 @@
+/*
+Zweck:      Einrichten der Word-Press Datenbank
+Autor:      Bischofberger Leonie, Riedener Samuel, Davatz Ben
+Datum:      09.12.2023
+*/
+
+-- Datenbank mit dem Namen Worpress erstellen
+-- Benutzer wordpress erstellen
+-- Benutzer wordpress Berechtigung Select, Insert, Update, Delete, Create, Drop, Alter
+-- Berechtigungen aktualisieren/aktivieren
+
 CREATE DATABASE wordpress;
 CREATE USER wordpress@localhost IDENTIFIED BY 'Pa55wort';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER
