8f8f96fb1689ec2d742e1e3e4013188b1deb1642
TinBlock50 <87497582+TinBlock50@users.noreply.github.com>
2023-12-09 16:10:04 +0100

    Passwort angepasst.
    Passwort gesetzt

--- Configs/MySQL_Setup.sql
+++ Configs/MySQL_Setup.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE wordpress;
-CREATE USER wordpress@localhost IDENTIFIED BY '<your-password>';
+CREATE USER wordpress@localhost IDENTIFIED BY 'Pa55wort';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER
   ON wordpress.*
   TO wordpress@localhost;
