4569e3ab4fd3044c589099375675942593a873ee
leonie04 <143601194+leonie04@users.noreply.github.com>
2023-12-13 15:58:11 +0100

    mysql auf seperate instance

--- installWordPress.sh
+++ installWordPress.sh
@@ -3,6 +3,13 @@ aws ec2 create-security-group --group-name wordpress-sec-group --description "EC
 aws ec2 authorize-security-group-ingress --group-name wordpress-sec-group --protocol tcp --port 80 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-name wordpress-sec-group --protocol tcp --port 443 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-name wordpress-sec-group --protocol tcp --port 22 --cidr 0.0.0.0/0
+
+aws ec2 run-instances --image-id ami-0fc5d935ebf8bc3bc --count 1 --instance-type t2.micro --key-name aws-wordpress-cli --security-groups wordpress-sec-group --iam-instance-profile Name=LabInstanceProfile --user-data file://initialmysql.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=mysql}]' --no-paginate
 aws ec2 run-instances --image-id ami-0fc5d935ebf8bc3bc --count 1 --instance-type t2.micro --key-name aws-wordpress-cli --security-groups wordpress-sec-group --iam-instance-profile Name=LabInstanceProfile --user-data file://initialWordPress.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=WordPress}]' --no-paginate
 chmod 600 ~/.ssh/aws-wordpress-cli.pem
+aws ec2 authorize-security-group-ingress --group-name wordpress-sec-group --protocol tcp --ipRanges  --cidr 0.0.0.0/32
+
 aws ec2 describe-instances --query "Reservations[*].Instances[*].{InstanceId:InstanceId, PublicIP: PublicIpAddress, State: State.Name}"
+
+#Filter nach Name
+aws ec2 describe-instances --filters "Name=tag:Name,Values=mysql" --query "Reservations[*].Instances[*].{PublicIP: PublicIpAddress}"
