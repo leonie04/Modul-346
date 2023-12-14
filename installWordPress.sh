ba498a61c2e01b76ddaf2075f3da46603ec361a8
leonie04 <143601194+leonie04@users.noreply.github.com>
2023-12-14 17:26:09 +0100

    Update installWordPress.sh
    Kommentare
    Anpassung der Securitygroup

--- installWordPress.sh
+++ installWordPress.sh
@@ -1,3 +1,7 @@
+#Schlüsselpaar und Sicherheitsgruppe erstellen
+#Ports freischalten
+#Instanzen starten
+#Securitygroup anpassen
 aws ec2 create-key-pair --key-name aws-wordpress-cli --key-type rsa --query 'KeyMaterial' --output text > ~/.ssh/aws-wordpress-cli.pem
 aws ec2 create-security-group --group-name wordpress-sec-group --description "EC2-WordPress-SG"
 aws ec2 authorize-security-group-ingress --group-name wordpress-sec-group --protocol tcp --port 80 --cidr 0.0.0.0/0
@@ -7,9 +11,8 @@ aws ec2 authorize-security-group-ingress --group-name wordpress-sec-group --prot
 aws ec2 run-instances --image-id ami-0fc5d935ebf8bc3bc --count 1 --instance-type t2.micro --key-name aws-wordpress-cli --security-groups wordpress-sec-group --iam-instance-profile Name=LabInstanceProfile --user-data file://initialmysql.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=mysql}]' --no-paginate
 aws ec2 run-instances --image-id ami-0fc5d935ebf8bc3bc --count 1 --instance-type t2.micro --key-name aws-wordpress-cli --security-groups wordpress-sec-group --iam-instance-profile Name=LabInstanceProfile --user-data file://initialWordPress.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=WordPress}]' --no-paginate
 chmod 600 ~/.ssh/aws-wordpress-cli.pem
-aws ec2 authorize-security-group-ingress --group-name wordpress-sec-group --protocol tcp --ipRanges  --cidr 0.0.0.0/32
 
-aws ec2 describe-instances --query "Reservations[*].Instances[*].{InstanceId:InstanceId, PublicIP: PublicIpAddress, State: State.Name}"
+public_ip=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=mysql" --query "Reservations[*].Instances[*].{PublicIP: PublicIpAddress}" --output json | jq -r '.[][].PublicIP')
+aws ec2 authorize-security-group-ingress --group-name launch-wizard-5 --protocol tcp --port 22 --cidr $public_ip/32
 
-#Filter nach Name
-aws ec2 describe-instances --filters "Name=tag:Name,Values=mysql" --query "Reservations[*].Instances[*].{PublicIP: PublicIpAddress}"
+
