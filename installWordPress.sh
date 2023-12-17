21a8b7a4223ef0bf3d4c6f0b8861cc99c30d7094
leonie04 <143601194+leonie04@users.noreply.github.com>
2023-12-17 08:04:05 +0100

    Update installWordPress.sh

--- installWordPress.sh
+++ installWordPress.sh
@@ -8,7 +8,7 @@ aws ec2 authorize-security-group-ingress --group-name wordpress-sec-group --prot
 aws ec2 authorize-security-group-ingress --group-name wordpress-sec-group --protocol tcp --port 443 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-name wordpress-sec-group --protocol tcp --port 22 --cidr 0.0.0.0/0
 
-aws ec2 run-instances --image-id ami-0fc5d935ebf8bc3bc --count 1 --instance-type t2.micro --key-name aws-wordpress-cli --security-groups wordpress-sec-group --iam-instance-profile Name=LabInstanceProfile --user-data file://initialmysql.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=mysql}]' --no-paginate
+aws ec2 run-instances --image-id ami-0fc5d935ebf8bc3bc --count 1 --instance-type t2.micro --key-name aws-wordpress-cli --security-groups wordpress-sec-group --iam-instance-profile Name=LabInstanceProfile --user-data file://initialMySQL.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MySQL}]' --no-paginate
 aws ec2 run-instances --image-id ami-0fc5d935ebf8bc3bc --count 1 --instance-type t2.micro --key-name aws-wordpress-cli --security-groups wordpress-sec-group --iam-instance-profile Name=LabInstanceProfile --user-data file://initialWordPress.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=WordPress}]' --no-paginate
 chmod 600 ~/.ssh/aws-wordpress-cli.pem
 
