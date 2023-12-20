fdb7b4d7fd24c2fb8dec8b98c11c63235885ac31
TinBlock50 <87497582+TinBlock50@users.noreply.github.com>
2023-12-20 18:27:22 +0100

    cp initialWordPress.txt
    cp initialWordPress.txt and use the copy to create the instance. This fixes problems when the installation gets interrupted.

--- installWordPress.sh
+++ installWordPress.sh
@@ -13,16 +13,14 @@ aws ec2 authorize-security-group-ingress --group-name wordpress-sec-group --prot
 echo start mysql instance
 aws ec2 run-instances --image-id ami-0fc5d935ebf8bc3bc --count 1 --instance-type t2.micro --key-name aws-wordpress-cli --security-groups wordpress-sec-group --iam-instance-profile Name=LabInstanceProfile --user-data file://initialMySQL.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MySQL_oie2ds45turo}]' | cat > MySQLInstance.log
 
+cp initialWordPress.txt initialWordPressLive.txt
 public_ip=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=MySQL_oie2ds45turo" --query "Reservations[*].Instances[*].{PublicIP: PublicIpAddress}" --output text | grep -v None)
-sed -i "s/Soll_DB_Host_IP/$public_ip/" initialWordPress.txt
+sed -i "s/Soll_DB_Host_IP/$public_ip/" initialWordPressLive.txt
 
 echo start wordpress instance
-aws ec2 run-instances --image-id ami-0fc5d935ebf8bc3bc --count 1 --instance-type t2.micro --key-name aws-wordpress-cli --security-groups wordpress-sec-group --iam-instance-profile Name=LabInstanceProfile --user-data file://initialWordPress.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=WordPress}]' | cat > WordPressInstance.log
+aws ec2 run-instances --image-id ami-0fc5d935ebf8bc3bc --count 1 --instance-type t2.micro --key-name aws-wordpress-cli --security-groups wordpress-sec-group --iam-instance-profile Name=LabInstanceProfile --user-data file://initialWordPressLive.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=WordPress}]' | cat > WordPressInstance.log
 chmod 600 ~/.ssh/aws-wordpress-cli.pem
 
 wp_ip=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=WordPress" --query "Reservations[*].Instances[*].{PublicIP: PublicIpAddress}" --output text | grep -v None)
-
-# Reset changes to preserve the original 
-sed -i "s/$public_ip/Soll_DB_Host_IP/" initialWordPress.txt
 
 echo wordpress is soon avaiable on $wp_ip
